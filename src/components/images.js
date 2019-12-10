e643fddbb304dbc989555007d94b732d7683baa6
Joesh Bautista <joeshbautista@csu.fullerton.edu>
2019-12-10 13:25:58 -0800

    Clean up

--- src/components/images.js
+++
@@ -1,7 +0,0 @@
-export const images = [
-  "https://wallpaperaccess.com/full/767277.jpg",
-  "https://wallpaperaccess.com/full/438099.jpg",
-  "http://www.hdnicewallpapers.com/Walls/Big/Food%20and%20Drinks/Fruit_and_Egg_Foods_4K_Wallpaper.jpg",
-  "https://s2.best-wallpaper.net/wallpaper/3840x2160/1806/Food-burgers-fried-potatoes-food_3840x2160.jpg",
-  "https://pixelz.cc/wp-content/uploads/2018/10/meat-and-vegetable-bbq-skewers-uhd-4k-wallpaper.jpg"
-];
